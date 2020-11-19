99bf2bc1a06d04003e8c265c778f1811167e7686
hj <dbghwns66@daum.net>
2020-11-20 03:02:06 +0900

    add:ch04

---
+++ ch04/convolution-prewitt-x.c
@@ -0,0 +1,54 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <Windows.h>
+//
+//#define MASK_SIZE 3
+//int main(void) {
+//	BITMAPFILEHEADER hf;
+//	BITMAPINFOHEADER hinfo;
+//	RGBQUAD hRGB[256];
+//	FILE* fp;
+//	int imgSize;
+//	int margin = MASK_SIZE / 2;
+//	int mask[MASK_SIZE][MASK_SIZE] = { {-1,0,1 }, {-1, 0, 1}, {-1, 0, 1} };
+//	double sum = 0.0;
+//
+//	fp = fopen("lenna.bmp", "rb");
+//	if (fp == NULL) {
+//		perror("fopen");
+//		exit(1);
+//	}
+//
+//	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+//	fread(&hinfo, sizeof(BITMAPINFOHEADER), 1, fp);
+//	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+//
+//	imgSize = hinfo.biWidth * hinfo.biHeight;
+//	BYTE* image = (BYTE*)malloc(imgSize);
+//	BYTE* output = (BYTE*)malloc(imgSize);
+//	fread(image, sizeof(BYTE), imgSize, fp);
+//	fclose(fp);
+//
+//	for (int i = margin; i < hinfo.biHeight - margin; i++) {
+//		for (int j = margin; j < hinfo.biWidth - margin; j++) {
+//			for (int m = -margin; m <= margin; m++) {
+//				for (int n = -margin; n <= margin; n++) {
+//					sum += image[(i + m) * hinfo.biWidth + (j + n)] * mask[margin+m][margin+n];
+//				}
+//			}
+//			output[i * hinfo.biWidth + j] = (BYTE)(abs(sum) / 3.0);
+//			sum = 0.0;
+//		}
+//	}
+//
+//	fp = fopen("output.bmp", "wb");
+//	fwrite(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+//	fwrite(&hinfo, sizeof(BITMAPINFOHEADER), 1, fp);
+//	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+//	fwrite(output, sizeof(BYTE), imgSize, fp);
+//	fclose(fp);
+//
+//	free(image);
+//	free(output);
+//
+//}
---
+++ ch04/convolution-prewitt.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#define MASK_SIZE 3
+int main(void) {
+	BITMAPFILEHEADER hf;
+	BITMAPINFOHEADER hinfo;
+	RGBQUAD hRGB[256];
+	FILE* fp;
+	int imgSize;
+	int margin = MASK_SIZE / 2;
+	int mask1[MASK_SIZE][MASK_SIZE] = {-1,-1,-1,0,0,0,1,1,1};
+	int mask2[MASK_SIZE][MASK_SIZE] = { -1,0,1,-1,0,1,-1,0,1 };
+	double sum = 0.0;
+
+	fp = fopen("lenna.bmp", "rb");
+	if (fp == NULL) {
+		perror("fopen");
+		exit(1);
+	}
+
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hinfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+
+	imgSize = hinfo.biWidth * hinfo.biHeight;
+	BYTE* image = (BYTE*)malloc(imgSize);
+	BYTE* output1 = (BYTE*)malloc(imgSize);
+	BYTE* output2 = (BYTE*)malloc(imgSize);
+	BYTE* result = (BYTE*)malloc(imgSize);
+	fread(image, sizeof(BYTE), imgSize, fp);
+	fclose(fp);
+
+	for (int i = margin; i < hinfo.biHeight - margin; i++) {
+		for (int j = margin; j < hinfo.biWidth - margin; j++) {
+			for (int m = -margin; m <= margin; m++) {
+				for (int n = -margin; n <= margin; n++) {
+					sum += image[(i + m) * hinfo.biWidth + (j + n)] * mask1[margin+m][margin+n];
+				}
+			}
+			output1[i*hinfo.biWidth+j] = (BYTE)(abs(sum) / 3.0);
+			sum = 0.0;
+		}
+	}
+	
+	for (int i = margin; i < hinfo.biHeight - margin; i++) {
+		for (int j = margin; j < hinfo.biWidth - margin; j++) {
+			for (int m = -margin; m <= margin; m++) {
+				for (int n = -margin; n <= margin; n++) {
+					sum += image[(i + m) * hinfo.biWidth + (j + n)] * mask2[margin + m][margin + n];
+				}
+			}
+			output2[i * hinfo.biWidth + j] = (BYTE)(abs(sum) / 3.0);
+			sum = 0.0;
+		}
+	}
+
+	for (int i = 0; i < imgSize; i++) {
+		if (output1[i] > 30||output2[i]>30)result[i] = 255;
+		else result[i] = 0;
+	}
+	fp = fopen("output.bmp", "wb");
+	fwrite(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fwrite(&hinfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(result, sizeof(BYTE), imgSize, fp);
+	fclose(fp);
+
+	free(image);
+	free(output1);
+	free(output2);
+	free(result);
+}
